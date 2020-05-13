b1a63c3e831694bf362f0a4f4bf08e0bd31525da
Muhammad Mashood Siddiquie <40804440+Mashood97@users.noreply.github.com>
2020-05-13 16:01:12 +0500

    Flutter explicit animations
    Explicit Animations in flutter

---
+++ android/app/src/main/kotlin/com/example/explicit_animation_flutter/MainActivity.kt
@@ -0,0 +1,6 @@
+package com.example.explicit_animation_flutter
+
+import io.flutter.embedding.android.FlutterActivity
+
+class MainActivity: FlutterActivity() {
+}
---
+++ lib/main.dart
@@ -0,0 +1,96 @@
+import 'package:flutter/material.dart';
+
+void main() {
+  runApp(MyApp());
+}
+
+class MyApp extends StatelessWidget {
+  // This widget is the root of your application.
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      title: 'Flutter Demo',
+      theme: ThemeData(
+        // This is the theme of your application.
+        //
+        // Try running your application with "flutter run". You'll see the
+        // application has a blue toolbar. Then, without quitting the app, try
+        // changing the primarySwatch below to Colors.green and then invoke
+        // "hot reload" (press "r" in the console where you ran "flutter run",
+        // or simply save your changes to "hot reload" in a Flutter IDE).
+        // Notice that the counter didn't reset back to zero; the application
+        // is not restarted.
+        primarySwatch: Colors.blue,
+        // This makes the visual density adapt to the platform that you run
+        // the app on. For desktop platforms, the controls will be smaller and
+        // closer together (more dense) than on mobile platforms.
+        visualDensity: VisualDensity.adaptivePlatformDensity,
+      ),
+      home: MyHomePage(),
+    );
+  }
+}
+
+class MyHomePage extends StatefulWidget {
+  @override
+  _MyHomePageState createState() => _MyHomePageState();
+}
+
+class _MyHomePageState extends State<MyHomePage>
+    with SingleTickerProviderStateMixin {
+  AnimationController _animationController;
+  Animation curvedAnimation;
+
+  @override
+  void initState() {
+    super.initState();
+    _animationController = AnimationController(
+      duration: Duration(seconds: 4),
+      vsync: this,
+    );
+
+    _animationController.repeat();
+    curvedAnimation =
+        CurvedAnimation(parent: _animationController, curve: Curves.bounceOut);
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    // This method is rerun every time setState is called, for instance as done
+    // by the _incrementCounter method above.
+    //
+    // The Flutter framework has been optimized to make rerunning build methods
+    // fast, so that you can just rebuild anything that needs updating rather
+    // than having to individually change instances of widgets.
+    return Scaffold(
+      body: Stack(
+        children: [
+          PositionedTransition(
+            child: Container(
+              height: 100,
+              width: 100,
+              decoration: BoxDecoration(
+                shape: BoxShape.circle,
+                color: Colors.blue,
+              ),
+            ),
+            rect: RelativeRectTween(
+                    begin: RelativeRect.fromLTRB(0, 0, 0, 700),
+                    end: RelativeRect.fromLTRB(0, 700, 200, 0))
+                .animate(curvedAnimation),
+          ),
+        ],
+      ),
+      floatingActionButton: FloatingActionButton(
+        onPressed: () {
+          if (_animationController.isAnimating) {
+            _animationController.stop();
+          } else {
+            _animationController.repeat();
+          }
+        },
+        child: Icon(Icons.book),
+      ),
+    );
+  }
+}
